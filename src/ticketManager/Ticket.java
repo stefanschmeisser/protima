094d6f3e600497a9ede49208244d0d38a9947dda
Oliver Kulas <oli@Olivers-MacBook-Pro.local>
2014-03-24 11:20:08 +0100

    Zwischenstand Klasse Ticket

--- src/ticketManager/Ticket.java
+++ src/ticketManager/Ticket.java
@@ -1,6 +1,22 @@
 package ticketManager;
 
+import org.eclipse.swt.widgets.DateTime;
+
+
+
+
 public class Ticket {
+	
+	private int ticketId;
+	private String ticketTitle;
+	private String ticketDesc;
+	private DateTime ticketStartDate;
+	private DateTime ticketEndDate;
+	private enum ticketPriorityLevel { INTIME, URGENT, CRITICAL}
+	private enum notifyStatus {}
+	private enum ticketProcessStatus {}
+	private int currentEditorUID;
+	private int assignedTeamID;
 	
 }
 /*
