176314fc12438f3edcdc8eb76a797e61be0a9204
Stefan Schmeisser <stefan.andreas.schmeisser@googlemail.com>
2014-04-04 21:23:16 +0200

    insert SQL querys in ProjectDAO + fix Project

--- src/projectManager/ProjectDaoMySql.java
+++ src/projectManager/ProjectDaoMySql.java
@@ -3,6 +3,7 @@ package projectManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.sql.PreparedStatement;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,18 +11,22 @@ import applicationManager.AbstractDaoMySql;
 
 public class ProjectDaoMySql extends AbstractDaoMySql implements IProjectDAO {
 
-	private Project project;
 	private List<Project> projectList;
 	
 	/**
 	 * creates a new project in the database
 	 */
 	@Override
-	public void createProject(){
+	public void createProject(String name, String description, int projectmanagerID){
 		try {
 			openConnection(_user, _password);
-			Statement stmt = connection.createStatement();
-			ResultSet rs = stmt.executeQuery("INSERT INTO project(projectID, name, description, projectmanagerID) VALUES ()");
+//			Statement stmt = connection.createStatement();
+//			ResultSet rs = stmt.executeQuery("INSERT INTO project(projectID, name, description, projectmanagerID) VALUES ()");
+			PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO project(name, description, projectmanagerID) VALUES (?,?,?)");
+			preparedStatement.setString(1, name);
+			preparedStatement.setString(2, description);
+			preparedStatement.setInt(3, projectmanagerID);
+			preparedStatement.executeUpdate();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
@@ -31,16 +36,33 @@ public class ProjectDaoMySql extends AbstractDaoMySql implements IProjectDAO {
 	 * edit an existing project in the database
 	 */
 	@Override
-	public void editProject(int id){
-		
+	public void editProject(int projectID, String name, String description, int projectmanagerID){
+		openConnection(_user, _password);
+		try {
+			PreparedStatement preparedStatement = connection.prepareStatement("UPDATE project SET name = ?, description = ?, projectmanagerID = ? WHERE projectID = ?");
+			preparedStatement.setString(1, name);
+			preparedStatement.setString(2, description);
+			preparedStatement.setInt(3, projectmanagerID);
+			preparedStatement.setInt(4, projectID);
+			preparedStatement.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	/**
 	 * delete an existing project from the database
 	 */
 	@Override
-	public void removeProject(int id){
-		
+	public void removeProject(int projectID){
+		openConnection(_user,_password);
+		try{
+			PreparedStatement preparedStatement = connection.prepareStatement("DELETE FROM project WHERE projectID = ?");
+			preparedStatement.setInt(1, projectID);
+			preparedStatement.executeUpdate();
+		}catch(SQLException e){
+			e.printStackTrace();
+		}
 	}
 	
 	/**
@@ -49,8 +71,21 @@ public class ProjectDaoMySql extends AbstractDaoMySql implements IProjectDAO {
 	@Override
 	public List<Project> getProjectList(){
 		this.projectList = new ArrayList<Project>();
-		
-		
+		openConnection(_user,_password);
+		try{
+			Statement statement = connection.createStatement(); 
+			ResultSet resultSet = statement.executeQuery("SELECT * FROM project");
+			while(resultSet.next()){
+				Project project = new Project();
+				project.setProjectId(resultSet.getInt("projectID"));
+				project.setProjectName(resultSet.getString("name"));
+				project.setProjectDescription(resultSet.getString("description"));
+				project.setProjectManager(resultSet.getInt("projectmanagerID"));
+				this.projectList.add(project);
+			}
+		}catch(SQLException e){
+			e.printStackTrace();
+		}
 		return this.projectList;
 	}
 	
@@ -59,9 +94,22 @@ public class ProjectDaoMySql extends AbstractDaoMySql implements IProjectDAO {
 	 * returns a existing project form the database
 	 */
 	@Override
-	public Project getProject(){
-		this.project = new Project();
-		
-		return this.project;
+	public Project getProject(int projectID){
+		Project project = new Project();
+		openConnection(_user,_password);
+		try{
+			PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM project WHERE projectID = ?");
+			preparedStatement.setInt(1, projectID);
+			ResultSet resultSet = preparedStatement.executeQuery();
+			while(resultSet.next()){
+				project.setProjectId(resultSet.getInt("projectID"));
+				project.setProjectName(resultSet.getString("name"));
+				project.setProjectDescription(resultSet.getString("description"));
+				project.setProjectManager(resultSet.getInt("projectmanagerID"));
+			}
+		}catch(SQLException e){
+			e.printStackTrace();
+		}
+		return project;
 	}	
 }
