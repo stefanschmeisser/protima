7be7ac177f1cd5b63934916bf365dcf9b29b05c9
Stefan Schmeisser <stefan.andreas.schmeisser@googlemail.com>
2014-04-11 13:51:42 +0200

    fix CONSTANT in protima.java

--- src/applicationManager/Protima.java
+++ src/applicationManager/Protima.java
@@ -4,8 +4,8 @@ public interface Protima {
 //	String user = "sag";
 //	String password = "Jr22HhUSLMz9Nu3m";
 //	String host = "jdbc:mysql://games.thm.de/sag";
-	String user = "root";
-	String password = "";
-	String host = "jdbc:mysql://localhost/sag";
+	public static final String USER = "root";
+	public static final String PASSWORD = "";
+	public static final String HOST = "jdbc:mysql://localhost/sag";
 }
 
