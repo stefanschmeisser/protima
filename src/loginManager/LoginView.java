c81f193ba14cf8e2b8d66614cfcde28a1d2421d6
MariusWeitzel <weitzel.marius@gmail.com>
2014-04-11 13:46:46 +0200

    Fehler Meldung und Icon implementiert

--- src/loginManager/LoginView.java
+++ src/loginManager/LoginView.java
@@ -1,9 +1,12 @@
 package loginManager;
 import org.eclipse.swt.*;
+import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.layout.*;
 import org.eclipse.swt.widgets.*;
 
 public class LoginView {
+	
+	private Shell shell;
 	
 	// noch alles andere als schön, aber läuft schon mal
 	//TODO: hoffentlich mit Layout erledigt: schöne Abstände um alles
@@ -18,8 +21,10 @@ public class LoginView {
 		
 //		final Display disp = new Display();
 		// kann minimiert & geschlossen werden, aber nicht vergrößert/verkleinert
-		final Shell shell = new Shell(disp, SWT.CLOSE | SWT.TITLE | SWT.MIN);
+		shell = new Shell(disp, SWT.CLOSE | SWT.TITLE | SWT.MIN);
 		shell.setText("Login");
+		Image imageApplicationIcon = new Image(Display.getCurrent(), "content/icon.png");
+        shell.setImage(imageApplicationIcon);
 		
 		GridLayout gridLayout = new GridLayout();
 		gridLayout.numColumns = 3;
@@ -34,8 +39,8 @@ public class LoginView {
 //		text1.setText("");
 //		text1.setBounds(70,10,200,20);
 		textUser.setTextLimit(30);
-		//FIXME: debug richtiger Benutzer!
-		textUser.setText("marius");
+		//FIXME: debug Benutzer!
+//		textUser.setText("marius");
 		GridData gd1 = new GridData(GridData.FILL_HORIZONTAL);
 		gd1.widthHint = 150;
 		gd1.horizontalSpan = 2;
@@ -48,8 +53,8 @@ public class LoginView {
 		
 		final Text textPassword = new Text(shell, SWT.NONE);
 		textPassword.setEchoChar('*');
-		//FIXME: debug falsches PW!
-		textPassword.setText("marius_pw");
+		//FIXME: debug PW!
+//		textPassword.setText("marius_pw");
 //		text2.setBounds(70,50,200,20);
 //		text2.setText("Password");
 		GridData gd2 = new GridData(GridData.FILL_HORIZONTAL);
@@ -79,8 +84,7 @@ public class LoginView {
 //					System.out.println("Enter pressed");
 //					System.out.println("Benutzer: " + textUser.getText() +"\tPassword: "+ textPassword.getText());
 					if (!papa.checkUserCredentials(textUser.getText(), textPassword.getText())) {
-						// FIXME: wo soll das später sonst ausgegeben werden?!
-						System.out.println("falsche Benutzerangaben");
+						errorBox();
 						textUser.setText("");
 						textPassword.setText("");
 						return;
@@ -99,8 +103,7 @@ public class LoginView {
 				if (event.widget == btn_ok) {
 //					System.out.println("Benutzer: " + textUser.getText() +"\tPassword: "+ textPassword.getText());
 					if (!papa.checkUserCredentials(textUser.getText(), textPassword.getText())) {
-						// FIXME: wo soll das später sonst ausgegeben werden?!
-						System.out.println("falsche Benutzerangaben");
+						errorBox();
 						textUser.setText("");
 						textPassword.setText("");
 						return;
@@ -133,6 +136,13 @@ public class LoginView {
 		if (_isLoggedIn[0]) {
 			parent.switchToFcState();
 		}
+	}
+	
+	private void errorBox() {
+		MessageBox dialog = new MessageBox(shell, SWT.APPLICATION_MODAL | SWT.OK | SWT.ICON_ERROR);
+		dialog.setText("Falsche Benutzerangaben");
+		dialog.setMessage("Falsche Benutzerangaben");
+		dialog.open();
 	}
 	
 }
